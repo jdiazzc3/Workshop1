015fe64cf1de888f765a3504ae60546faaeac379
jdiazzc3 <jddiazz.036@gmail.com>
2024-08-13 20:35:39 -0500

    first commit

---
+++ src/main/java/co/com/Diaz/DiazApplication.java
@@ -0,0 +1,13 @@
+package co.com.Diaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/Diaz/DiazApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.Diaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
